bd4f48d1be90ba845a5036cf31cdd242ebb15463
Sabuhi <sabuhi@sabuhi.com>
2024-03-29 17:32:26 +0400

    Configure File

--- app/utils/create_super_user.py
+++ app/utils/create_super_user.py
@@ -2,19 +2,37 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 from app.main import app, db, Users
+from colorama import Fore
 
 
 def username_exists(username):
     return db.session.query(db.exists().where(Users.username == username)).scalar()
 
 
-def createadmin(username, password):
-    with app.app_context():
-        if username_exists(username):
-            print("Username already exists")
-        else:
-            admin = Users(username=username, firstname='admin', lastname='admin', usermode='admin')
-            admin.set_password(password)
-            db.session.add(admin)
-            db.session.commit()
-            print(f"{username} user created successfully.")
+def control_credentials(username, password):
+    if not (2 < len(username) < 13):
+        return False, "Username must be between 3 and 12 characters long"
+
+    if not (5 < len(password) < 16):
+        return False, "Password must be between 6 and 15 characters long"
+
+    return True, "Credentials are valid."
+
+
+def createsuperuser(username, password):
+    valid, message = control_credentials(username, password)
+    if valid:
+        with app.app_context():
+            if username_exists(username):
+                print(Fore.LIGHTRED_EX, "Username already exists, try different username", Fore.RESET)
+            else:
+                admin = Users(username=username,
+                              firstname=username,
+                              lastname=username,
+                              usermode='admin')
+                admin.set_password(password)
+                db.session.add(admin)
+                db.session.commit()
+                print(Fore.LIGHTGREEN_EX, f"{username} user created successfully.", Fore.RESET)
+    else:
+        print(Fore.RED, message, Fore.RESET, sep='')
--- manage.py
+++ manage.py
@@ -1,30 +1,35 @@
 from app.main import app
-from app.utils.createadmin import createadmin
+from app.utils.create_super_user import createsuperuser
 import argparse
 
 
-def run_server():
+def start_server():
     app.run(host='0.0.0.0', port=5000, debug=True)
 
 
-def create_admin(username, password):
-    createadmin(username, password)
+def create_super_user(username, password):
+    createsuperuser(username, password)
 
 
 def main():
     parser = argparse.ArgumentParser(description='Run various scripts')
-    parser.add_argument('command', choices=['runserver', 'createadmin'], help='Command to execute')
-    parser.add_argument('--username', type=str, help='Admin username')
-    parser.add_argument('--password', type=str, help='Admin password')
+    subparsers = parser.add_subparsers(dest='command', help='Available commands')
+
+    parser_startserver = subparsers.add_parser('startserver', help='Start the Flask server',
+                                             description='Starts the Flask server')
+    parser_createsuperuser = subparsers.add_parser('createsuperuser', help='Create a superuser',
+                                                   description='Creates a superuser in the database')
+    parser_createsuperuser.add_argument('--username', type=str, help='Superuser Username', required=True)
+    parser_createsuperuser.add_argument('--password', type=str, help='Superuser Password', required=True)
 
     args = parser.parse_args()
 
-    if args.command == 'runserver':
-        run_server()
-    elif args.command == 'createadmin':
+    if args.command == 'startserver':
+        start_server()
+    elif args.command == 'createsuperuser':
         if not args.username or not args.password:
-            parser.error("Username and password are required for 'createadmin' command.")
-        create_admin(args.username, args.password)
+            parser.error("Username and password are required for 'createsuperuser' command")
+        create_super_user(args.username, args.password)
 
 
 if __name__ == '__main__':
